4fcdb2ddd7a389630f4d580482112372763a2787
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2025-11-09 17:24:41 -0500

    exploring color and font combinations

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,9 @@ function App() {
   return (
     <>
       <div>
-        <h1 style={{ fontFamily: "PlayfairDisplay" }}>
-          PlayfairDisplay - Main Header (i.e. page name)
-        </h1>
-        <h2 style={{ fontFamily: "NotoSans" }}>
-          NotoSans Regular - Sub Headers (About, Skills, Resume, ect)
-        </h2>
-        <p style={{ fontFamily: "Montserrat" }}>
-          Montserrat Regular (all regular text)
-        </p>
+        <h1>PlayfairDisplay - Main Header (i.e. page name)</h1>
+        <h2>Montserrat Regular - Sub Headers (About, Skills, Resume, ect)</h2>
+        <p>Montserrat Regular (all regular text)</p>
       </div>
     </>
   );
