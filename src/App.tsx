e8dcb10e47b2f08bbbffe3e4f85f659b339aa381
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2025-11-09 20:39:59 -0500

    experimenting with layout and basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,18 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div>
-        <h1>PlayfairDisplay - Main Header (i.e. page name)</h1>
-        <h2>Montserrat Regular - Sub Headers (About, Skills, Resume, ect)</h2>
-        <p>Montserrat Regular (all regular text)</p>
+      <header>
+        <h1>Dicky Kitchen Jr - Developer Extraordinaire</h1>
+        <p className="h1-subheader">
+          The talented developer with the unique name.
+        </p>
+      </header>
+      <div className="content-tabs">
+        <p className="content-tab-left grid-1">Skills</p>
+        <p className="content-tab-right grid-2">About</p>
+        <p className="content-tab-left grid-3">Certifications</p>
+        <p className="content-tab-right grid-4">Resume</p>
+        <p className="content-tab-left grid-5">Contact</p>
       </div>
     </>
   );
